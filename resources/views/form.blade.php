4664f84099a85e77295f7068bfe9874db130f6c1
Pradeep <pradeepvjvargiya@gmail.com>
2023-08-18 16:45:57 +0530

    Form page Updated

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -9,14 +9,14 @@
             @method('PUT')
         @endisset
         <div class="mb-4">
-            <label for="title">Title</label>
+            <label for="title">Title:</label>
             <input type="text" name="title" id="title" @class(['border-red-500' => $errors->has('title')]) value="{{ $task->title ?? old('title') }}" />
             @error('title')
                 <p class="error">{{ $message }}</p>
             @enderror
         </div>
         <div class="mb-4">
-            <label for="description">Description</label>
+            <label for="description">Description:</label>
             <textarea name="description" id="description" @class(['border-red-500' => $errors->has('description')]) rows="5">{{ $task->description ?? old('description') }}</textarea>
             @error('description')
                 <p class="error">{{ $message }}</p>
